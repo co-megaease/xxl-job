e572815d206b1acfc4678f5df2e20581f2587da6
Jason Peng <jasonjpeng@foxmail.com>
2021-09-14 15:48:40 +0800

    set jobLogId and triggerType into job execute param

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/dto/DefaultParam.java
@@ -0,0 +1,25 @@
+package com.xxl.job.admin.core.dto;
+
+
+public class DefaultParam {
+
+    private Long xxlJobLogId;
+
+    private String triggerType;
+
+    public Long getXxlJobLogId() {
+        return xxlJobLogId;
+    }
+
+    public void setXxlJobLogId(Long xxlJobLogId) {
+        this.xxlJobLogId = xxlJobLogId;
+    }
+
+    public String getTriggerType() {
+        return triggerType;
+    }
+
+    public void setTriggerType(String triggerType) {
+        this.triggerType = triggerType;
+    }
+}
